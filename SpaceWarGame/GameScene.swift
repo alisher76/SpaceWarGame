5a56ee798b61e1c30aff22b70b1051833984302d
Ali•Gorithm <alisher.coder@gmail.com>
2017-03-04 16:03:53 -0500

    FirstStageComplete

--- SpaceWarGame/GameScene.swift
+++ SpaceWarGame/GameScene.swift
@@ -8,6 +8,7 @@
 
 import SpriteKit
 import GameplayKit
+import CoreMotion
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
@@ -23,6 +24,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     var gameTimer:Timer!
+    var possibleAliens = ["alien", "alien2", "alien3"]
+    let alienCategory:UInt32 = 0x1 << 1
+    let photonTorpedoCategory:UInt32 = 0x1 << 0
+    
+    let motionManager = CMMotionManager()
+    var xAcceleration: CGFloat = 0
+    
     
     
     override func didMove(to view: SKView) {
@@ -48,29 +56,166 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         scoreLabel.fontName = "AmericanTypewriter-Bold"
         scoreLabel.fontSize = 36
         scoreLabel.fontColor = UIColor.white
+        score = 0
         
         self.addChild(scoreLabel)
         
         
+        gameTimer = Timer.scheduledTimer(timeInterval: 0.75, target: self, selector: #selector(addAlien), userInfo: nil, repeats: true)
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        motionManager.accelerometerUpdateInterval = 0.2
+        motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { (data:CMAccelerometerData?, error: Error?) in
+            if let accelerometerData = data {
+            
+                let acceleraion = accelerometerData.acceleration
+                self.xAcceleration = CGFloat(acceleraion.x) * 2 + self.xAcceleration * 0.10
+            }
+            
+        }
         
         
         
         
            }
     
+    func addAlien() {
+    
+        //Func
+        possibleAliens = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: possibleAliens) as! [String]
+        
+        let alien = SKSpriteNode(imageNamed: possibleAliens[0])
+        let randomAlienPosition = GKRandomDistribution(lowestValue: 0, highestValue: 1440)
+        let position = CGFloat(randomAlienPosition.nextInt())
+        alien.position = CGPoint(x: position, y: self.frame.size.height + alien.size.height)
+        alien.physicsBody = SKPhysicsBody(rectangleOf: alien.size)
+        alien.physicsBody?.isDynamic = true
+        
+        alien.physicsBody?.categoryBitMask = alienCategory
+        alien.physicsBody?.contactTestBitMask = photonTorpedoCategory
+        alien.physicsBody?.collisionBitMask = 0
+        
+        self.addChild(alien)
+        
+        let animationDuration = 6
+        
+        var actionArray = [SKAction]()
+        
+        actionArray.append(SKAction.move(to: CGPoint(x: position, y: alien.size.height), duration: TimeInterval(animationDuration)))
+        actionArray.append(SKAction.removeFromParent())
+        
+        alien.run(SKAction.sequence(actionArray))
+        
+        
+        
+        
+        
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        fireTorpedo()
+    }
+    
+    func fireTorpedo() {
+    
+        self.run(SKAction.playSoundFileNamed("torpedo.mp3", waitForCompletion: false))
+        
+        let torpedoNode = SKSpriteNode(imageNamed: "torpedo")
+        torpedoNode.position = player.position
+        torpedoNode.position.y += 5
+        
+        torpedoNode.physicsBody = SKPhysicsBody(circleOfRadius: torpedoNode.size.width / 2)
+        torpedoNode.physicsBody?.isDynamic = true
+        
+        torpedoNode.physicsBody?.categoryBitMask = photonTorpedoCategory
+        torpedoNode.physicsBody?.contactTestBitMask = alienCategory
+        torpedoNode.physicsBody?.collisionBitMask = 0
+        torpedoNode.physicsBody?.usesPreciseCollisionDetection = true
+        
+        
+        self.addChild(torpedoNode)
+        
+        let animationDuration:TimeInterval = 0.3
+        
+        var actionArray = [SKAction]()
+        
+        actionArray.append(SKAction.move(to: CGPoint(x: player.position.x, y: self.frame.size.height + 10), duration: TimeInterval(animationDuration)))
+        actionArray.append(SKAction.removeFromParent())
+        
+        torpedoNode.run(SKAction.sequence(actionArray))
+
+    }
+    
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        var firstBody:SKPhysicsBody
+        var secondBody:SKPhysicsBody
+        
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+        
+            firstBody = contact.bodyA
+            secondBody = contact.bodyB
+        }else{
+        
+            firstBody = contact.bodyB
+             secondBody = contact.bodyA
+        
+        }
+        
+        if (firstBody.categoryBitMask & photonTorpedoCategory) != 0 && (secondBody.categoryBitMask & alienCategory) != 0 {
+        torpedoDidCollideWithAlien(torpedoNode: firstBody.node as! SKSpriteNode, alienNode: secondBody.node as! SKSpriteNode)
+         
+        }
+    }
+    
+    func torpedoDidCollideWithAlien(torpedoNode:SKSpriteNode, alienNode:SKSpriteNode) {
+    
+        let explosion = SKEmitterNode(fileNamed: "Explosion")!
+        explosion.position = alienNode.position
+        self.addChild(explosion)
+        
+        self.run(SKAction.playSoundFileNamed("explosion.mp3", waitForCompletion: false))
+        
+        torpedoNode.removeFromParent()
+        alienNode.removeFromParent()
+        
+        
+        
+        self.run(SKAction.wait(forDuration: 2)) {
+        explosion.removeFromParent()
+        }
+     
+        score += 5
+    }
+    
+    
+    override func didSimulatePhysics() {
+        
+        player.position.x += xAcceleration * 50
+        
+        if player.position.x < -10 {
+            player.position = CGPoint(x: self.size.width + 10, y: player.position.y)
+        }else if player.position.x > self.size.width + 10 {
+            player.position = CGPoint(x: -10, y: player.position.y)
+        }
+        
+    }
+    
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
